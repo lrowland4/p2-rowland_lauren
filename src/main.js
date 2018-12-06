5659e6ed78e45ee674cd45ccb7803f0ca0cdaf66
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-12-06 17:06:01 -0500

    Add Fortune generator to Javascript

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const colorButtons = document.querySelectorAll('#colors #rb #red, #colors #rb #blue, #colors #gy #yellow, #colors #gy #green, #numbers #starter #red, #numbers #starter #blue, #numbers #alldone #green, #numbers #alldone #yellow');
-const colorList = document.querySelector('.colors');
+const colorList = document.querySelector('#colors');
 const numberList = document.querySelector('#number');
 
 function wrapper(){
@@ -46,6 +46,58 @@ section.addEventListener('click', function slideToggle(){
 
 
 /////////////////////////////////////////////
+
+let fortuneOptions = ['You will be very rich.', 'You will be very poor.', 'You will have 10 children.', 'You will have 2 children.', 'You will marry someone famous.', 'You will live in a small house.', 'You will live in a Mansion.', 'You will soon come into a lot of money.', 'You will soon lose a lot of money.', 'You are admired by everyone.'];
+
+const choice = document.getElementsByClassName('choice') [0];
+const theFortune = document.querySelector('#numbers starter alldone');
+
+const fortuneTextPlaceholder = document.createTextNode('');
+yourFortune.appendChild(fortuneTextPlaceholder);
+
+
+
+const choiceSelection = document.getElementById('generator-button');
+
+
+let fortuneTeller = function() {
+  
+  fortuneOptions.sort(function(a, b){return 0.5 - Math.random()});
+
+  var fortuneText = document.createTextNode(fortuneOptions[0]);
+    fortuneTextPlaceholder.appendChild(fortuneText);
+
+    ourButtonSection.removeChild(button);
+
+    theFortune.classList.add('fortune-show');
+
+
+};
+
+
+
+button.onclick = fortuneTeller;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
