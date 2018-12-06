a42d5bcf9d3afaa66e178d6cc1d660a429b24177
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-12-06 15:30:46 -0500

    Add Flashing to Numbers

--- src/main.js
+++ src/main.js
@@ -40,3 +40,39 @@ section.addEventListener('click', function slideToggle(){
   
   article.classList.toggle('hide');
 });
+
+
+/////////////////////////////////////////////
+
+const numberButtons = document.querySelectorAll('#numbers #12 #red, #numbers #12 #blue, #numbers #34 #yellow, #numbers #34 #green');
+const numberList = document.querySelector('#numbers');
+
+function wrapper(){
+
+  const changeColor = () => {
+     colorList.classList.toggle('black');
+
+  };
+
+  const loopThrough = () => {
+
+    let thisButtonText = this.childNodes[0].nodeValue;
+    let thisButtonLength = thisButtonText.length;
+
+
+    for (let i = 0; i < thisButtonLength * 2; i++) {
+      setTimeout(function() {
+        changeColor();
+       }, i * 1000);
+
+    }
+
+  };
+  loopThrough();
+};
+
+for(let i = 0; i < numberButtons.length; i++) {
+  numberButtons[i].addEventListener('click', wrapper);
+
+};
+
