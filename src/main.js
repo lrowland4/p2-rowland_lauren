1cd9e21339f229dc9274e8fe9539dc38cb8eee3f
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-12-06 13:25:13 -0500

    Add Javascript to make words flash based on length

--- src/main.js
+++ src/main.js
@@ -1,1 +1,31 @@
+const colorButtons = document.querySelectorAll('#colors #rb #red, #colors #rb #blue, #colors #gy #yellow, #colors #gy #green');
+const colorList = document.querySelector('#colors');
 
+function wrapper(){
+
+	const changeColor = () => {
+  	 colorList.classList.toggle('black');
+
+  };
+
+  const loopThrough = () => {
+
+  	let thisButtonText = this.childNodes[0].nodeValue;
+  	let thisButtonLength = thisButtonText.length;
+
+
+    for (let i = 0; i < thisButtonLength * 2; i++) {
+      setTimeout(function() {
+      	changeColor();
+       }, i * 1000);
+
+    }
+
+  };
+  loopThrough();
+};
+
+for(let i = 0; i < colorButtons.length; i++) {
+	colorButtons[i].addEventListener('click', wrapper);
+
+};
