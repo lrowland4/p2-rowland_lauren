39fc69d8dd24a4a5574d6f541e3bd74d4d92d2f2
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-12-06 20:24:08 -0500

    Fix code for better functionality

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
-const colorButtons = document.querySelectorAll('#colors #rb #red, #colors #rb #blue, #colors #gy #yellow, #colors #gy #green, #numbers #starter #red, #numbers #starter #blue, #numbers #alldone #green, #numbers #alldone #yellow');
 const colorList = document.querySelector('#colors');
-const numberList = document.querySelector('#number');
+const colorList2 = document.querySelector('#numbers');
+const colorList3 = document.querySelector('#numbers2');
+const colorButtons = document.querySelectorAll('#colors #rb #red, #colors #rb #blue, #colors #gy #yellow, #colors #gy #green');
+const colorButtons2 = document.querySelectorAll('#numbers #starter #red, #numbers #starter #blue, #numbers #alldone #green, #numbers #alldone #yellow');
+const colorButtons3 = document.querySelectorAll('#numbers2 #lastnum #red, #numbers2 #lastnum #blue, #numbers2 #finalnum #green, #numbers2 #finalnum #yellow');
+
+
+
 
 function wrapper(){
 
 	const changeColor = () => {
   	 colorList.classList.toggle('black');
-     numberList.classList.toggle('black');
+    
 
-  };
+  }; 
 
 
   const loopThrough = () => {
@@ -32,72 +38,5 @@ for(let i = 0; i < colorButtons.length; i++) {
 	colorButtons[i].addEventListener('click', wrapper);
 
 };
-
-
-/////////////////////////////////////////////
-
-const article = document.getElementsByTagName('article')[0];
-const section = document.getElementsByTagName('section')[0];
-
-section.addEventListener('click', function slideToggle(){
-  
-  article.classList.toggle('hide');
-});
-
-
-/////////////////////////////////////////////
-
-let fortuneOptions = ['You will be very rich.', 'You will be very poor.', 'You will have 10 children.', 'You will have 2 children.', 'You will marry someone famous.', 'You will live in a small house.', 'You will live in a Mansion.', 'You will soon come into a lot of money.', 'You will soon lose a lot of money.', 'You are admired by everyone.'];
-
-const choice = document.getElementsByClassName('choice') [0];
-const theFortune = document.querySelector('#numbers starter alldone');
-
-const fortuneTextPlaceholder = document.createTextNode('');
-yourFortune.appendChild(fortuneTextPlaceholder);
-
-
-
-const choiceSelection = document.getElementById('generator-button');
-
-
-let fortuneTeller = function() {
-  
-  fortuneOptions.sort(function(a, b){return 0.5 - Math.random()});
-
-  var fortuneText = document.createTextNode(fortuneOptions[0]);
-    fortuneTextPlaceholder.appendChild(fortuneText);
-
-    ourButtonSection.removeChild(button);
-
-    theFortune.classList.add('fortune-show');
-
-
-};
-
-
-
-button.onclick = fortuneTeller;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
