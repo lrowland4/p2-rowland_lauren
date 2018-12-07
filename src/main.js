e5091475e742068ad90c2b99944f9f2413959cc4
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-12-06 21:00:30 -0500

    Add functionality to the second set of numbers

--- src/main.js
+++ src/main.js
@@ -40,9 +40,94 @@ function wrapper(){
   loopThrough();
 };
 
+
+
+///////////////////////////////////////////////////////////////////
+
+
+
+function wrapper2(){
+  const changeColor2 = () => {
+     colorList2.classList.toggle('black');
+  }; 
+
+  const loopThrough2 = () => {
+    let thisNumber = this.childNodes[0].nodeValue;
+    let numberValue = Number(thisNumber);
+
+
+    for (let i = 0; i < numberValue * 2; i++) {
+      setTimeout(function() {
+        changeColor2();
+       }, i * 500);
+
+    }
+
+    setTimeout(function() {
+     colorList2.classList.add('hide');
+     colorList2.classList.remove('show');
+     colorList3.classList.remove('hide');
+     colorList3.classList.add('show');
+   }, numberValue * 1000);
+
+
+
+  };
+  loopThrough2();
+};
+
+
+
+
+
+///////////////////////////////////////////////////////////////////
+
+
+
+function wrapper3(){
+  const changeColor3 = () => {
+     colorList3.classList.toggle('black');
+  }; 
+
+  const loopThrough3 = () => {
+    let thisNumber = this.childNodes[0].nodeValue;
+    let numberValue = Number(thisNumber);
+
+
+    for (let i = 0; i < numberValue * 2; i++) {
+      setTimeout(function() {
+        changeColor3();
+       }, i * 500);
+
+    }
+
+    setTimeout(function() {
+     colorList3.classList.add('hide');
+     colorList3.classList.remove('show');
+   }, numberValue * 1000);
+
+
+
+  };
+  loopThrough3();
+};
+
+////////////////////////////////////////////
+
+
 for(let i = 0; i < colorButtons.length; i++) {
 	colorButtons[i].addEventListener('click', wrapper);
 
 };
 
+
+for(let i = 0; i < colorButtons.length; i++) {
+  colorButtons2[i].addEventListener('click', wrapper2);
+
+};
+
+for(let i = 0; i < colorButtons.length; i++) {
+  colorButtons3[i].addEventListener('click', wrapper3);
+
+};
 
