26d305298a7a0e90b40d3b6f1f018e0f59e77902
Lauren Rowland <Laurenrowland@Laurens-MBP-6.lan>
2018-12-06 21:56:00 -0500

    Update JavaScript attempt to show

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const colorButtons3 = document.querySelectorAll('#numbers2 #lastnum #red3, #numb
 ///////////////////////////////////////
 
 
-const magicFortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
+const theFortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 
 const button = document.getElementsByTagName('button')[0];
 const yourFortune = document.querySelector('#fortune-wrapper p span');
@@ -18,29 +18,17 @@ const fortuneTextPlaceholder = document.createTextNode('');
 yourFortune.appendChild(fortuneTextPlaceholder);
 
 
-let shakeBall = function() {
+let fortuneTeller = function() {
   
-  magicFortunes.sort(function(a, b){return 0.5 - Math.random()});
+  theFortunes.sort(function(a, b){return 0.5 - Math.random()});
 
-  let fortuneText = document.createTextNode(magicFortunes[0]);
+  let fortuneText = document.createTextNode(theFortunes[0]);
     yourFortune.appendChild(fortuneText);
 
     ourButtonSection.removeChild(button);
 
-    yourFortune.classList.add('fortune-show');
-
 
 };
-
-
-
-
-
-button.onclick = shakeBall;
-
-
-
-
 
 
 
@@ -145,7 +133,7 @@ function wrapper3(){
     setTimeout(function() {
      colorList3.classList.add('hide');
      colorList3.classList.remove('show');
-     shakeBall.classList.add('slide-in');
+     fortuneTeller.classList.add('fortune-show');
    }, numberValue * 1000);
 
 
